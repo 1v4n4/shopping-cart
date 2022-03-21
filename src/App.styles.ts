8e7da176c0e8018cfa3a3ad868413083ca12dc24
1v4n4 <ivananl@yahoo.com>
2022-03-21 21:36:38 +0100

    Add cart drawer and button

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,3 +1,12 @@
 import styled from "styled-components";
+import { IconButton } from "@mui/material";
 
-export const Wrapper = styled.div``;
+export const Wrapper = styled.div`
+margin: 2rem;`;
+
+export const StyledButton = styled(IconButton)`
+position: fixed;
+top: 0;
+z-index: 10;
+right: 2rem;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,33 @@
-import { Wrapper } from "./App.styles";
+import { useState } from "react";
+import { Wrapper, StyledButton } from "./App.styles";
 import { useQuery } from "react-query";
-import { LinearProgress } from "@mui/material";
+import { LinearProgress, Badge } from "@mui/material";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import Grid from "@mui/material/Grid";
+import Drawer from "@mui/material/Drawer";
 import { CartItemType } from "./types";
 import { getProducts } from "./api";
 import Item from './item/Item';
 import {handleAddItem } from './helpers';
 
 const App = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItemType[]>([]);
+
   const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
   console.log(data);
   if (isLoading) return <LinearProgress />;
   if (error) return <div>Error</div>;
   return (
     <Wrapper>
-
+      <Drawer anchor='right' open={isOpen} onClose={()=>setIsOpen(false)}>
+Cart here
+      </Drawer>
+      <StyledButton onClick={() => setIsOpen(true)}>
+        <Badge color='error'>
+          <AddShoppingCartIcon />
+        </Badge>
+      </StyledButton>
     <Grid container spacing={3}>
         {data?.map((item) => (
           <Grid item key={item.id} xs={12} sm={4}>
